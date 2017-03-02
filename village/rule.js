75232e78799e919879b6ffdba41723378bba954f
mh <mh.northlander@gmail.com>
2017-03-02 15:53:24 +0900

    role set func

--- village/rule.js
+++ village/rule.js
@@ -2,33 +2,69 @@
 module.exports = Rule;
 
 // imports
-firstNightSee = require("../role/seer").firstNightSee;
-
+role = require("../role/");
 
 // rule
 function Rule(){
     var rule = Object.create(Rule.prototype);
 
+    // time for discussion in the daytime (sec)
     rule.dayTime = 1;
+    // modify dayTime each day for convinience
     rule.dayTimeDecreasesBy = 1;
-
+    // time for discussion or action in the night (sec)
     rule.nightTime = 1;
 
-    rule.firstNightSee = firstNightSee.Given;
+    // how Seer acts in the first night. (None / Given / Choice)
+    rule.firstNightSee = role.Seer.firstNightSee.Given;
+    // the first victim (NPC) can have role
     rule.roleLackable = false;
+
+    // number of members. minimum 4
+    member = 4;
+    // set of role in this village. Map(name -> num)
+    roleSet = role.defaultRoleSet(member);
 
     return rule;
 };
 
 Rule.prototype = {
-    update: function(dayTime, dayTimeDecreasesBy, nightTime, firstNightSee, roleLackable){
-        if(dayTime){ this.dayTime = dayTime; }
-        if(dayTimeDecreasesBy){ this.dayTimeDecreasesBy = dayTimeDecreasesBy; }
+    updateBase: function(q){
+        if(q.dayTime){ this.dayTime = q.dayTime; }
+        if(q.dayTimeDecreasesBy){ this.dayTimeDecreasesBy = q.dayTimeDecreasesBy; }
 
-        if(nightTime){ this.nightTime = nightTime; }
+        if(q.nightTime){ this.nightTime = q.nightTime; }
 
-        if(firstNightSee){ this.firstNightSee = firstNightSee; }
-        if(roleLackable){ this.roleLackable = roleLackable; }
+        if(q.firstNightSee){ this.firstNightSee = q.firstNightSee; }
+        if(q.roleLackable){ this.roleLackable = q.roleLackable; }
+    },
+    updateRoleSet: function(m){ // m: Map name -> num
+        for(var name of role.roleNameList){
+            if(m.has(name)){
+                this.roleSet.set(name, m.get(name));
+            }
+        }
+    },
+
+    JSONToRoleSet: function(obj){
+        m = new Map();
+        for(var name of role.roleNameList){
+            m.set(name, obj[name] ? obj[name] : 0)
+        }
+        return m;
+    },
+    roleSetJSON: function(){
+        o = {};
+        for(var [name,n] of this.roleSet){
+            o.name = n;
+        }
+        return o;
+    },
+
+    toJSON: function(){
+        ret = Object.assgin({}, this);
+        ret.roleSet = this.roleSetJSON();
+        return ret;
     },
 };
 
